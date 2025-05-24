9a7763b2d2d2c1dc2a86d14adb7e9c165d65120a
Andrew Bobylev <igr-srn@mail.ru>
2025-05-24 11:12:09 +0300

    изменил вывод ответа в экшене store и пробросил данные ответа в маршрутизацию ответа

--- app/Http/Controllers/FormProcessor.php
+++ app/Http/Controllers/FormProcessor.php
@@ -12,11 +12,10 @@ class FormProcessor extends Controller
     }
     public function store(Request $request)
     {
-        $response = [
-            'first name' => $request->get('firstname'),
-            'last name' => $request->get('lastname'),
-            'email' => $request->get('email'),
+        $data = [
+            'firstname' => $request->get('firstname'),
+            'lastname' => $request->get('lastname'),
         ];
-        return response()->json($response, 200, [], JSON_UNESCAPED_UNICODE);
+        return view('usergreeting', ['response' => $data]);
     }
 }
