ef1486c790d3eb296e1a5a8e98dfe248fbe5db3f
Andrew Bobylev <igr-srn@mail.ru>
2025-05-24 10:40:15 +0300

    создан шаблон формы, экшн и маршрут для отображения данной формы

---
+++ app/Http/Controllers/FormProcessor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FormProcessor extends Controller
+{
+    public function index()
+    {
+        return view('userform');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FormProcessor;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/userform', [FormProcessor::class, 'index']);
